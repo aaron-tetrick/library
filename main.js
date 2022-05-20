5c3f6af63f2cb0b343235d00ff33137dd9c9358a
Aaron Tetrick <atetrick14102@gmail.com>
2022-05-20 04:20:28 -0400

    Corrected deleteEntry function, working on toggleRead function next

--- main.js
+++ main.js
@@ -11,13 +11,14 @@ const libraryHeader = document.querySelector(".book-item-header")
 
 
 //EVENT LISTENERS
-form.addEventListener('submit', runEvent)
-form.addEventListener('submit', () => addBookToLibrary(title.value, author.value, pages.value, read.value))
+form.addEventListener('submit', runEvent);
+form.addEventListener('submit', () => addBookToLibrary(title.value, author.value, pages.value, read.value));
+
 
 
 //CREATES EMPTY LIBRARY
 let myLibrary = [];
-
+console.log(myLibrary);
 //PREVENTS DEFAULT 
 function runEvent(e) {
     e.preventDefault();
@@ -29,15 +30,6 @@ function Book(title, author, pages, read) {
     this.bookAuthor = author;
     this.bookPages = pages;
     this.bookRead = read;
-   // let displayBook1 = function() {
-
-  //  }
-    console.log(title);
-    console.log(this);
-    console.log(this.title)
-    console.log(this.bookTitle)
-    console.log(this.newTitle)
-    console.log(this.bookAuthor)
 };
 
 //console.log(Book("One", 'Me', '69', 'have-read'))
@@ -47,44 +39,161 @@ const addBookToLibrary = function(newTitle, newAuthor, newPages, newRead) {
     const bookObj = new Book(newTitle, newAuthor, newPages, newRead);
     myLibrary.push(bookObj);
     displayBook(bookObj);
+    console.log(myLibrary);
 };
 
 addBookToLibrary.prototype = Object.create(Book.prototype);
 
+let newBookLine;
+let newTitle;
+let newAuthor;
+let newPages;
+let newRead;
+let newReadBtn;
+let deleteLine;
+let deleteBtn;
 
 
 const displayBook = function(newBook) {
+        newBookLine = document.createElement('tr');
+        tbody.appendChild(newBookLine);
 
-        let newTitle = document.createElement('tr');
+        //Create individual cells for each books' details
+        newTitle = document.createElement('td');
+        newAuthor = document.createElement('td');
+        newPages = document.createElement('td');
+        newRead = document.createElement('td');
+        newReadBtn = document.createElement('button');
+        deleteLine = document.createElement('td');
+        deleteBtn = document.createElement('button');
+
+        //Give newBookLine and cells correct styling
+        newBookLine.className = "book-line";
+        newTitle.className = "book-item";
+        newAuthor.className = "book-item";
+        newPages.className = "book-item";
+        newRead.className = "book-item";
+        deleteLine.className = "book-item";
+        deleteLine.className = "delete-item";
+        deleteBtn.className = "delete-btn";
+
+        //Give book cells the correct information to display on page
+        newTitle.innerText = `${newBook.bookTitle}`;
+        newAuthor.innerText = `${newBook.bookAuthor}`;
+        newPages.innerText = `${newBook.bookPages}`;
+        if (newBook.bookRead === 'have-read') {
+            newReadBtn.innerText = 'Have Read';
+            newReadBtn.className = 'read-button-true';
+      } else if (newBook.bookRead === 'not-read') {
+        newReadBtn.innerText = 'Not Read'
+        newReadBtn.className = 'read-button-false'
+      };
+
+
+      //Append book cells and buttons to page
+        newBookLine.appendChild(newTitle);
+        newBookLine.appendChild(newAuthor);
+        newBookLine.appendChild(newPages);
+        newBookLine.appendChild(newRead);
+        newRead.appendChild(newReadBtn);
+        newBookLine.appendChild(deleteLine);
+        deleteBtn.appendChild(document.createTextNode('X'));
+        deleteLine.appendChild(deleteBtn);
+
+//Loop through myLibrary Array and assign each book entry an id that matches it's placement in the library.
     for (i=0; i < myLibrary.length; i++) {
-        console.log(newBook.bookTitle, "HIIIIII");
-        return;
-        // console.log(this);
-        // console.log(this.Book);
-        // console.log(this.Book.prototype);
-        // console.log(this.addBookToLibrary);
+        newBookLine.setAttribute('id', i);
     }
-    // console.log(this.bookAuthor);
-    // console.log(Book);
-    // console.log(Book.prototype);
-    // console.log(addBookToLibrary.prototype)
+  
+
+newReadBtn.addEventListener('click', toggleRead);
+deleteBtn.addEventListener('click', deleteEntry);
+
+
+console.log(tbody);
+console.log(myLibrary);
+
+
+
+}
+
+displayBook.prototype = Object.create(Book)
+
+//Delete an entry from the webpage, delete the corresponding myLibrary index, reset the id attribute
+function deleteEntry(e) {
+    //Creates an array
+    let entryArray;
+
+    if(e.target.classList.contains('delete-btn')) {
+        //If confirm, remove the entry from webpage
+        if(confirm('Are You Sure?')){
+            let bookEntry = e.target.parentElement.parentElement;
+            tbody.removeChild(bookEntry);
+        }
+
+         //Removes entry from Library Array
+         for (i=0; i < myLibrary.length; i++) {
+            myLibrary.splice(newBookLine.id, 1);
+            console.log(newBookLine.id);
+        }
+
+        //Removes ID from each entry that is left
+        entryArray = Array.from(tbody.children);
+        console.log(entryArray);
+        entryArray.forEach(entry => {
+            entry.removeAttribute('id');
+        });
+
+        //Adds new, correct order ID back for each entry
+        for (i=0; i < entryArray.length; i++) {
+            console.log(entryArray);
+            if (entryArray[i].classList.contains("book-line")) {
+                entryArray[i].setAttribute('id', (i-1));
+            };
+    };
+
+    console.log(entryArray);
+    console.log(myLibrary);
+}
 }
 
 
-displayBook.prototype = Object.create(Book.prototype)
-console.dir(displayBook);
-console.dir(addBookToLibrary);
-console.dir(Book)
-console.dir(displayBook.prototype);
-console.dir(addBookToLibrary.prototype);
-console.dir(Book.prototype);
+//Toggle read button
+function toggleRead(e) {
+        if(e.target.classList.contains("read-button-true")) {
+        e.target.classList.toggle("read-button-true");
+        e.target.classList.toggle("read-button-false");
+        newReadBtn.innerText = 'Not Read'
+        myLibrary[newBookLine.id].bookRead = "not-read";
+        } else if (e.target.classList.contains("read-button-false")) {
+            e.target.classList.toggle("read-button-false");
+            e.target.classList.toggle("read-button-true");
+            newReadBtn.innerText = 'Have Read';
+            myLibrary[newBookLine.id].bookRead = "have-read";
+        }
+        console.log(myLibrary[newBookLine.id].bookRead);
+        console.log(myLibrary);
+    }
+
+//     if(entry.classList.contains('book-line')) {
+//         entry.setAttribute('id', i)
+//  }
+            
+        //     console.log(newBookLine);
+        //     console.log(tbody);
+        // }
+        // for(i=0; i < myLibrary.length; i++) {
+        // newBookLine.setAttribute('id', i);
+        // console.log(newBookLine, "Help")
+        // console.log(tbody);
+        // }
 
 //X// I want my book entry to be entered into the myLibrary array 
-//I want to display the myLibrary array onto the page
-//I want to press the read-btn and change the status on the page and in the myLibrary array
-//^^^Locate the bookObject by the myLibray index then change myLibrary[i].Book/bookObj.read = "have-read" or "not-read"
+//X//I want to display the myLibrary array onto the page
+////I want to press the read-btn and change the status on the page and in the myLibrary array
+////^^^Locate the bookObject by the myLibray index then change myLibrary[i].Book/bookObj.read = "have-read" or "not-read"
 
-//I want to press the delete-btn and delete the entry on the page and in the myLibrary array
+//X//I want to press the delete-btn and delete the entry on the page and in the myLibrary array
 //^^^Locate the bookObject by the myLibrary index then myLibrary.slice()/splice() <-- one of those
 //Create a loop in the displayBook function and loop through the myLibrary and use this.bookTitle, this.bookAuthor, etc.
 //Try the forEach loop, appending each to the page.
@@ -97,44 +206,7 @@ console.dir(Book.prototype);
 
 
 // function displayBook(title, author, pages, read) {
-//     let newBookLine = document.createElement('tr')
-//     tbody.appendChild(newBookLine);
 
-//     let newTitle = document.createElement('td');
-//     let newAuthor = document.createElement('td');
-//     let newPages = document.createElement('td');
-//     let newRead = document.createElement('td');
-//     let newReadBtn = document.createElement('button');
-//     let deleteLine = document.createElement('td');
-//     let deletebtn = document.createElement('button');
-
-//     newBookLine.className = "book-line";
-//     newTitle.className = "book-item";
-//     newAuthor.className = "book-item";
-//     newPages.className = "book-item";
-//     newRead.className = "book-item";
-//     deleteLine.className = "book-item";
-//     deleteLine.className = "delete-item";
-//     deletebtn.className = "delete-btn";
-
-//     newTitle.innerText = `${title}`;
-//     newAuthor.innerText = `${author}`;
-//     newPages.innerText = `${pages}`;
-//     if (read === 'have-read') {
-//     newReadBtn.innerText = 'Have Read';
-//     newReadBtn.className = 'read-button-true';    
-// } else if (read === 'not-read') {
-//     newReadBtn.innerText = 'Not Read'
-//     newReadBtn.className = 'read-button-false'
-// };
-//     newBookLine.appendChild(newTitle);
-//     newBookLine.appendChild(newAuthor);
-//     newBookLine.appendChild(newPages);
-//     newBookLine.appendChild(newRead);
-//     newRead.appendChild(newReadBtn);
-//     newBookLine.appendChild(deleteLine);
-//     deletebtn.appendChild(document.createTextNode('X'));
-//     deleteLine.appendChild(deletebtn);
     
 // deletebtn.addEventListener('click', DeleteBook);
 // newReadBtn.addEventListener('click', toggleRead);
