aacbd51d0b6c89ca912940d8edaf1babcd04ea94
Aaron Tetrick <atetrick14102@gmail.com>
2022-05-22 03:40:11 -0400

    Add credits and organize odd

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const read = document.getElementById("read");
 const form = document.querySelector('form');
 const tbody = document.querySelector('.tbody');
 
-
 //EVENT LISTENERS
 form.addEventListener('submit', runEvent);
 form.addEventListener('submit', () => addBookToLibrary(title.value, author.value, pages.value, read.value));
@@ -39,64 +38,61 @@ addBookToLibrary.prototype = Object.create(Book.prototype);
 
 //DISPLAYS NEW BOOK ENTRY ON THE PAGE
 const displayBook = function(newBook) {
-        let newBookLine = document.createElement('tr');
-        tbody.appendChild(newBookLine);
+    let newBookLine = document.createElement('tr');
+    tbody.appendChild(newBookLine);
 
-        //Create individual cells for each books' details
-        let newTitle = document.createElement('td');
-        let newAuthor = document.createElement('td');
-        let newPages = document.createElement('td');
-        let newRead = document.createElement('td');
-        let newReadBtn = document.createElement('button');
-        let deleteLine = document.createElement('td');
-        let deleteBtn = document.createElement('button');
+    //Create individual cells for each books' details
+    let newTitle = document.createElement('td');
+    let newAuthor = document.createElement('td');
+    let newPages = document.createElement('td');
+    let newRead = document.createElement('td');
+    let newReadBtn = document.createElement('button');
+    let deleteLine = document.createElement('td');
+    let deleteBtn = document.createElement('button');
 
-        //Give newBookLine and cells correct styling
-        newBookLine.className = "book-line";
-        newTitle.className = "book-item";
-        newAuthor.className = "book-item";
-        newPages.className = "book-item";
-        newRead.className = "book-item";
-        newReadBtn.className = "read-btn";
-        deleteLine.className = "book-item";
-        deleteLine.className = "delete-item";
-        deleteBtn.className = "delete-btn";
+    //Give newBookLine and cells correct styling
+    newBookLine.className = "book-line";
+    newTitle.className = "book-item";
+    newAuthor.className = "book-item";
+    newPages.className = "book-item";
+    newRead.className = "book-item";
+    newReadBtn.className = "read-btn";
+    deleteLine.className = "book-item";
+    deleteLine.className = "delete-item";
+    deleteBtn.className = "delete-btn";
 
-        //Give book cells the correct information to display on page
-        newTitle.innerText = `${newBook.bookTitle}`;
-        newAuthor.innerText = `${newBook.bookAuthor}`;
-        newPages.innerText = `${newBook.bookPages}`;
-        if (newBook.bookRead === 'have-read') {
-            newReadBtn.innerText = 'Have Read';
-            newReadBtn.className = 'read-button-true';
-      } else if (newBook.bookRead === 'not-read') {
-        newReadBtn.innerText = 'Not Read'
-        newReadBtn.className = 'read-button-false'
-      };
+    //Give book cells the correct information to display on page
+    newTitle.innerText = `${newBook.bookTitle}`;
+    newAuthor.innerText = `${newBook.bookAuthor}`;
+    newPages.innerText = `${newBook.bookPages}`;
+    if (newBook.bookRead === 'have-read') {
+        newReadBtn.innerText = 'Have Read';
+        newReadBtn.className = 'read-button-true';
+    } else if (newBook.bookRead === 'not-read') {
+    newReadBtn.innerText = 'Not Read'
+    newReadBtn.className = 'read-button-false'
+    };
 
-      //Append book cells and buttons to page
-        newBookLine.appendChild(newTitle);
-        newBookLine.appendChild(newAuthor);
-        newBookLine.appendChild(newPages);
-        newBookLine.appendChild(newRead);
-        newRead.appendChild(newReadBtn);
-        newBookLine.appendChild(deleteLine);
-        deleteBtn.appendChild(document.createTextNode('X'));
-        deleteLine.appendChild(deleteBtn);
+    //Append book cells and buttons to page
+    newBookLine.appendChild(newTitle);
+    newBookLine.appendChild(newAuthor);
+    newBookLine.appendChild(newPages);
+    newBookLine.appendChild(newRead);
+    newRead.appendChild(newReadBtn);
+    newBookLine.appendChild(deleteLine);
+    deleteBtn.appendChild(document.createTextNode('X'));
+    deleteLine.appendChild(deleteBtn);
 
 //Loop through myLibrary Array and assign each book entry an id that matches it's placement in the library.
     for (i=0; i < myLibrary.length; i++) {
         newBookLine.setAttribute('id', i);
     };
   
-
 newReadBtn.addEventListener('click', toggleRead);
 deleteBtn.addEventListener('click', deleteEntry);
-
 };
 
 displayBook.prototype = Object.create(Book)
-
 
 //Delete an entry from the webpage, delete the corresponding myLibrary index, reset the id attribute
 function deleteEntry(e) {
@@ -117,7 +113,6 @@ function deleteEntry(e) {
             entry.removeAttribute('id');
         });
 
-
         //Adds new ID, in the correct order, for each entry
         for (i=0; i < entryArray.length; i++) {
             if (entryArray[i].classList.contains("book-line")) {
@@ -125,6 +120,8 @@ function deleteEntry(e) {
             };
         };
     };
+    console.log(myLibrary);
+    console.log(tbody);
 };
 
 //Toggle read button
@@ -140,14 +137,6 @@ function toggleRead(e) {
             e.target.innerText = 'Have Read';
             myLibrary[e.target.parentElement.parentElement.id].bookRead = "have-read";
         };
+        console.log(myLibrary);
+        console.log(tbody);
     };
-
-//X// I want my book entry to be entered into the myLibrary array 
-//X//I want to display the myLibrary array onto the page
-////I want to press the read-btn and change the status on the page and in the myLibrary array
-////^^^Locate the bookObject by the myLibray index then change myLibrary[i].Book/bookObj.read = "have-read" or "not-read"
-
-//X//I want to press the delete-btn and delete the entry on the page and in the myLibrary array
-//^^^Locate the bookObject by the myLibrary index then myLibrary.slice()/splice() <-- one of those
-//Create a loop in the displayBook function and loop through the myLibrary and use this.bookTitle, this.bookAuthor, etc.
-//Try the forEach loop, appending each to the page.
